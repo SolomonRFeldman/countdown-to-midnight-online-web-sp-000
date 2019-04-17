97e7c5d7209bb357645abacfb00b6ab1cdb66799
Solomon Feldman <solomonrfeldman@gmail.com>
2019-04-17 11:25:34 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,8 @@
 
 def countdown(number)
   while number > 0
-    number -= 1
     "#{number} SECOND(S)!"
-    # number > 0 ? number -= 1 : number += 1
+    number -= 1
   end
   "HAPPY NEW YEAR!"
 end
